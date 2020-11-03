efe41e009f17f5d41dceec989c6da0577b295534
soltani <amirmohhamadsoltani@gmail.com>
2020-11-04 00:31:17 +0330

    add operaion except with test file

--- src/except.ts
+++ src/except.ts
@@ -1,7 +1,10 @@
+import clone from './clone'
+
 /**
 Returns a clone of the object minus the specified properties.
-@param {object} - the input object
-@param {string|string[]} - a single property, or array of properties to omit
+@param {object} ob - the input object
+@param {string|string[]} keys- a single property, or array of properties to omit
+@param {boolean=} deleteFromRe If the value is correct, the main object comes with the output. default false
 @returns {object}
 @static
 @example
@@ -10,11 +13,23 @@ Returns a clone of the object minus the specified properties.
 > o.except({ a: 1, b: 2, c: 3}, ['b', 'a'])
 { c: 3 }
 */
-export default function except<T extends { [key: string]: any }> (
+function except<
+  T extends { [key: string]: any },
+  K extends (keyof T | string) | Array<keyof T | string>
+>(
   ob: T,
-  keys: string | string[]
-) {
+  keys: K,
+  deleteFromRef: boolean = false
+): { [key in Exclude<keyof T, K extends string ? K : K[keyof K]>]: T[key] } {
+  const output = deleteFromRef ? ob : clone(ob)
   if (Array.isArray(keys)) {
-      
-  }
+    let i = keys.length
+    while (--i > -1) {
+      if (keys[i] in output) delete output[keys[i]]
+    }
+    return output
+  } else if (keys in output) delete output[<keyof T>keys]
+  return output
 }
+
+export default except
